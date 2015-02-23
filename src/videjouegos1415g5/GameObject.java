d10e9323347a3c9d148b73fd595ddb92fbc86e60
valarion <valarionch@gmail.com>
2015-02-23 17:57:15 +0100

    Base grafica

--- src/videjouegos1415g5/GameObject.java
+++ src/videjouegos1415g5/GameObject.java
@@ -1,16 +1,27 @@
 package videjouegos1415g5;
 
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
 public class GameObject {
 	
 	protected Rectangle position;
+	protected AnimationInstance animation;
 	
-	public GameObject(int x, int y, int size) {
+	public GameObject(int x, int y, int size, Animation animation) {
 		position = new Rectangle(x,y,size,size);
+		this.animation = new AnimationInstance(animation);
 	}
 	
-	public boolean interstects(GameObject o) {
+	public boolean intersects(GameObject o) {
 		return position.intersects(o.position);
+	}
+	
+	public void update(long ms) {
+		animation.update(ms);
+	}
+	
+	public void draw(Graphics2D g) {
+		animation.draw(g,position);
 	}
 }
