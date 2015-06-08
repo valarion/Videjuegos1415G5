892fb93c0c2485ce5b754635573c22571bb8d0fc
Arafo <rafa_marcen@hotmail.com>
2015-06-08 23:19:49 +0200

    Añadida música de fondo y efecto de sonido a las bombas

--- src/videjouegos1415g5/sound/Sound.java
+++ src/videjouegos1415g5/sound/Sound.java
@@ -4,38 +4,33 @@ import java.applet.Applet;
 import java.applet.AudioClip;
 
 public class Sound {
-	public static final Sound title = new Sound("");
-	public static final Sound intro = new Sound("");
-	
-	private boolean running;
+
+	public static final Sound boom = new Sound("/music/boom.wav");
+
 	private AudioClip clip;
 
 	private Sound(String name) {
 		try {
 			clip = Applet.newAudioClip(Sound.class.getResource(name));
-			running = false;
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void play() {
-		if (!running) {
-			try {
-				new Thread() {
-					public void run() {
-						clip.play();
-					}
-				}.start();
-			} catch (Throwable e) {
-				e.printStackTrace();
-			}
-			running = true;
+		try {
+			new Thread() {
+				public void run() {
+					clip.play();
+				}
+			}.start();
+		} catch (Throwable e) {
+			e.printStackTrace();
 		}
+
 	}
-	
+
 	public void stop() {
 		clip.stop();
-		running = false;
 	}
 }
