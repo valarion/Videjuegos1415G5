95cf60ed7529680fb941664515aefc4fd6f03d47
Arafo <rafa_marcen@hotmail.com>
2015-06-08 10:05:28 +0200

    Los eneminos no se mueven al morir

--- src/videjouegos1415g5/entity/Enemy.java
+++ src/videjouegos1415g5/entity/Enemy.java
@@ -9,23 +9,24 @@ import videjouegos1415g5.map.Map;
 import videjouegos1415g5.map.Obstacle;
 
 public class Enemy extends Mob {
-	
+
 	int xdir, ydir, speed = 1;
 
 	private GenerateObstacles obs;
 	private Bomberman player;
 	protected int score;
-	
+
 	private static Random r = new Random();
-	
+
 	protected Rectangle lastpos;
 
 	public Enemy(GenerateObstacles obs, Map map, Bomberman player) {
 		this.obs = obs;
 		this.player = player;
-		while (!located) findStartPos(map);
-		
-		switch(r.nextInt(3)) {
+		while (!located)
+			findStartPos(map);
+
+		switch (r.nextInt(3)) {
 		case 0:
 			xdir = speed;
 			ydir = 0;
@@ -44,59 +45,56 @@ public class Enemy extends Mob {
 			break;
 		}
 	}
-	
+
 	public void tick() {
-		if (!removed) animation.tick();
-		if (health <= 0) die(); 
-		
-		this.position.x += xdir;
-		this.position.y += ydir;
-		
-		boolean isinsquare = position.equals(lastpos);
-		lastpos = new Rectangle(position);
-		if(!isinsquare) for(Obstacle rect : obs.getPath()) {
-			if(rect.getBounds().contains(position)) {
-				isinsquare = true;
-				break;
+		if (!removed)
+			animation.tick();
+		if (health <= 0)
+			die();
+		else {
+			this.position.x += xdir;
+			this.position.y += ydir;
+
+			boolean isinsquare = position.equals(lastpos);
+			lastpos = new Rectangle(position);
+			if (!isinsquare)
+				for (Obstacle rect : obs.getPath()) {
+					if (rect.getBounds().contains(position)) {
+						isinsquare = true;
+						break;
+					}
+				}
+
+			if (isinsquare) {
+				switch (r.nextInt(30)) {
+				case 0:
+					xdir = speed;
+					ydir = 0;
+					break;
+				case 1:
+					xdir = -speed;
+					ydir = 0;
+					break;
+				case 2:
+					xdir = 0;
+					ydir = speed;
+					break;
+				case 3:
+					xdir = 0;
+					ydir = -speed;
+					break;
+				}
 			}
 		}
-		
-		if(isinsquare) {
-			switch(r.nextInt(30)) {
-			case 0:
-				xdir = speed;
-				ydir = 0;
-				break;
-			case 1:
-				xdir = -speed;
-				ydir = 0;
-				break;
-			case 2:
-				xdir = 0;
-				ydir = speed;
-				break;
-			case 3:
-				xdir = 0;
-				ydir = -speed;
-				break;
-			}
-		}
-		/*else {
-			// IA muy rudimentaria
-			if (player.position.x > this.position.x) {
-				this.position.x++;
-			} else {
-				this.position.x--;
-			}
-			
-			if (player.position.y > this.position.y) {
-				this.position.y++;
-			} else {
-				this.position.y--;
-			}
-		}*/
+		/*
+		 * else { // IA muy rudimentaria if (player.position.x >
+		 * this.position.x) { this.position.x++; } else { this.position.x--; }
+		 * 
+		 * if (player.position.y > this.position.y) { this.position.y++; } else
+		 * { this.position.y--; } }
+		 */
 	}
-	
+
 	public void touchedBy(Entity entity) {
 		if (health > 0 && entity instanceof Bomberman) {
 			entity.hurt(this, 10); // BIEN
@@ -104,12 +102,15 @@ public class Enemy extends Mob {
 	}
 
 	public boolean findStartPos(Map map) {
-		located: for (int y = 0; y < map.getmapHeight() - obs.getOffsetY()*2; y++) {
-			for (int x = 0; x < map.getmapWidth() - obs.getOffsetX()*2; x++) {
+		located: for (int y = 0; y < map.getmapHeight() - obs.getOffsetY() * 2; y++) {
+			for (int x = 0; x < map.getmapWidth() - obs.getOffsetX() * 2; x++) {
 				if (x != 0 && y != 0 || x != 1 && y != 0 || x != 0 && y != 1) {
-					if (!obs.obstacleAt(x, y, scale) && random.nextDouble() >= 0.99) {
-						this.position.x = x*obs.tileSize() + obs.getOffsetX()*obs.tileSize() - 8*scale;
-						this.position.y = y*obs.tileSize() + obs.getOffsetY()*obs.tileSize() + 24*scale;
+					if (!obs.obstacleAt(x, y, scale)
+							&& random.nextDouble() >= 0.99) {
+						this.position.x = x * obs.tileSize() + obs.getOffsetX()
+								* obs.tileSize() - 8 * scale;
+						this.position.y = y * obs.tileSize() + obs.getOffsetY()
+								* obs.tileSize() + 24 * scale;
 						located = true;
 						break located;
 					}
@@ -118,22 +119,22 @@ public class Enemy extends Mob {
 		}
 		return true;
 	}
-	
-	
+
 	public int getScore() {
 		return score;
 	}
-	
-	public static Enemy createEnemy(int type, GenerateObstacles obs, Map map, Bomberman player) {
-		switch(type) {
+
+	public static Enemy createEnemy(int type, GenerateObstacles obs, Map map,
+			Bomberman player) {
+		switch (type) {
 		case 0:
-			return new Balloon(obs,map,player);
+			return new Balloon(obs, map, player);
 		case 1:
-			return new BalloonPurple(obs,map,player);
+			return new BalloonPurple(obs, map, player);
 		case 2:
-			return new BalloonBlue(obs,map,player);
+			return new BalloonBlue(obs, map, player);
 		case 3:
-			return new BalloonRed(obs,map,player);
+			return new BalloonRed(obs, map, player);
 		}
 		return null;
 	}
