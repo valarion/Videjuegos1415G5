aaf0d8d5e0aa60e989a7940ee9823ab519c3196e
Arafo <rafa_marcen@hotmail.com>
2015-06-10 11:16:12 +0200

    Cuando bomberman es invencible parpadea en blanco

--- src/videjouegos1415g5/entity/Bomberman.java
+++ src/videjouegos1415g5/entity/Bomberman.java
@@ -1,5 +1,6 @@
 package videjouegos1415g5.entity;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
@@ -7,6 +8,7 @@ import java.awt.image.BufferedImage;
 import videjouegos1415g5.InputHandler;
 import videjouegos1415g5.animation.Animation;
 import videjouegos1415g5.animation.Animation.Direction;
+import videjouegos1415g5.gfx.Colors;
 import videjouegos1415g5.gfx.ScaleImg;
 import videjouegos1415g5.gfx.SpriteLoader;
 import videjouegos1415g5.gfx.SpriteSheet;
@@ -175,6 +177,13 @@ public class Bomberman extends Mob {
 				// g.fillRect(position.x+position.width/2-f.getWidth()/2,
 				// position.y+position.height/2-f.getHeight()/2, w*scale, h*scale);
 				g.drawImage(animation.getSprite(),
+						position.x + position.width / 2 - (f.getWidth() - 1 * scale)
+								/ 2, position.y + position.height / 2
+								- (f.getHeight() + 11 * scale) / 2, null);
+			}
+			else {
+				BufferedImage f = animation.getSprite();
+				g.drawImage(Colors.convertColor(f, Color.WHITE),
 						position.x + position.width / 2 - (f.getWidth() - 1 * scale)
 								/ 2, position.y + position.height / 2
 								- (f.getHeight() + 11 * scale) / 2, null);
